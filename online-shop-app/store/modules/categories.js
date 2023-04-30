bbeb6903ed5b5cb1593fa360130acbb5e46ee209
Marina_Pasternak <31963187+MarinaPasternak@users.noreply.github.com>
2023-04-30 23:21:51 +0300

    fixed spaces

--- online-shop-app/store/modules/categories.js
+++ online-shop-app/store/modules/categories.js
@@ -1,17 +1,17 @@
 export const state = () => ({
     categories: []
-  })
+})
   
-  export const mutations = {
+export const mutations = {
     SET_CATEGORIES(state, categories) {
       state.categories = categories;
     }
-  }
+}
   
-  export const actions = {
+export const actions = {
     async fetchAllCategories({ commit }) {
       const response = await fetch('https://dummyjson.com/products/categories');
       const categories = await response.json();
       commit('SET_CATEGORIES', categories)
     }
-  }
+}
